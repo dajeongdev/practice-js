2db9f3d2340e7e5c6b616051a5564b313d92ce3e
orangenbleu <orangenbleu@gmail.com>
2022-01-22 16:38:57 +0900

    finish make the drawing!

---
+++ js/DreamCoding/array.js
@@ -0,0 +1,79 @@
+'use strict';
+
+// 1. Declaration
+const arr1 = new Array();
+const arr2 = [1, 2];
+
+// 2. Index position
+const fruits = ['apple', 'banana'];
+console.log(fruits);
+console.log(fruits.length);
+console.log(fruits[0]);
+console.log(fruits[fruits.length - 1]);
+
+// 3. Looping over an array
+// print all fruits
+// a. for
+for (let i = 0; i < fruits.length; i++) {
+    console.log(fruits[i]);
+}
+
+// b. for of
+for (let fruit of fruits) {
+    console.log(fruit);
+}
+
+// c. forEach
+fruits.forEach((fruit) => {
+    console.log(fruit);
+});
+fruits.forEach(fruit => console.log(fruit));
+
+// 4. Addition, deletion, copy
+// push: add an item to the end
+fruits.push('grape', 'fig');
+console.log(fruits);
+
+// pop: remove an item from the end
+fruits.pop();
+fruits.pop();
+console.log(fruits);
+
+// unshift: add an item to the beginning
+fruits.unshift('peach');
+fruits.unshift('lemon');
+console.log(fruits);
+// shift: remove an item from the beginning
+fruits.shift();
+fruits.shift();
+console.log(fruits);
+
+// slice: remove an item by index position
+fruits.push('peach', 'strawberry', 'lemon');
+console.log(fruits);
+fruits.splice(1, 2, 'fig', 'watermelon');
+console.log(fruits);
+
+// combine two arrays
+const fruits2 = ['pear', 'coconut'];
+const newFruits = fruits.concat(fruits2);
+console.log(newFruits);
+
+// 5. Searching
+// indexOf: find the index
+console.clear();
+console.log(fruits);
+console.log(fruits.indexOf('apple'));
+console.log(fruits.indexOf('watermelon'));
+console.log(fruits.indexOf('coconut'));
+
+// includes
+console.log(fruits.includes('watermelon'));
+console.log(fruits.includes('coconut'));
+
+// lastIndexOf
+console.clear();
+console.log(fruits);
+fruits.push('apple');
+console.log(fruits.indexOf('apple'));
+console.log(fruits.lastIndexOf('apple'));

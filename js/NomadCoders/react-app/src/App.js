d0588c7b5404f6bbc1947ffd6e3063aa6d50a53a
orangenbleu <orangenbleu@gmail.com>
2022-01-31 16:38:52 +0900

    Finish Effect

--- js/NomadCoders/react-app/src/App.js
+++ js/NomadCoders/react-app/src/App.js
@@ -2,28 +2,21 @@ import Button from "./Button";
 import styles from "./App.module.css"
 import { useState, useEffect } from "react";
 
-function App() {
-  const [counter, setValue] = useState(0);
-  const [keyword, setKeyword] = useState("");
-  const onClick = () => setValue((prev) => prev + 1);
-  const onChange = (event) => setKeyword(event.target.value);
+function Hello() {
   useEffect(() => {
-    console.log('I run only once.');
+    console.log("hi :)");
+    return () => console.log("bye *("); // 언제 컴포넌트를 삭제했는지 알 수 있다! (자주 사용하진 않음)
   }, []);
-  useEffect(() => {
-    console.log("I run when 'keyword' changes.");
-  }, [keyword]);
-  useEffect(() => {
-    console.log("I run when 'counter' changes.");
-  }, [counter]);
-  useEffect(() => {
-    console.log("I run when keyword & counter changes.");
-  }, [keyword, counter]);
+  return <h1>Hello</h1>;
+}
+
+function App() {
+  const [showing, setShowting] = useState(false);
+  const onClick = () => setShowting((prev) => !prev);
   return (
     <div>
-      <input type="text" onChange={onChange} value={keyword} placeholder="Search here!" />
-      <h1 className={styles.title}>Total Count: {counter}</h1>
-      <button onClick={onClick}>Click Me!</button>
+      {showing ? <Hello /> : null}
+      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
     </div>
   );
 }

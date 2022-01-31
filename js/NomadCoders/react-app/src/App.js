f058d7430e59af93675c56bd589c0cf5723fb84b
orangenbleu <orangenbleu@gmail.com>
2022-01-31 16:24:37 +0900

    Add useEffect

--- js/NomadCoders/react-app/src/App.js
+++ js/NomadCoders/react-app/src/App.js
@@ -7,11 +7,18 @@ function App() {
   const [keyword, setKeyword] = useState("");
   const onClick = () => setValue((prev) => prev + 1);
   const onChange = (event) => setKeyword(event.target.value);
-  console.log('I run all the time');
   useEffect(() => {
-    console.log('Call The API');
+    console.log('I run only once.');
   }, []);
-  console.log("[SEARCH FOR]", keyword);
+  useEffect(() => {
+    console.log("I run when 'keyword' changes.");
+  }, [keyword]);
+  useEffect(() => {
+    console.log("I run when 'counter' changes.");
+  }, [counter]);
+  useEffect(() => {
+    console.log("I run when keyword & counter changes.");
+  }, [keyword, counter]);
   return (
     <div>
       <input type="text" onChange={onChange} value={keyword} placeholder="Search here!" />
